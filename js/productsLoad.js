01675bb391f54349efbd2f48bdecf9fe6e2e833f
Nicx A <nicx92@gmail.com>
2023-09-30 05:43:45 +0530

    Mod Wa Msg

--- js/productsLoad.js
+++ js/productsLoad.js
@@ -7,18 +7,8 @@ export async function Func(section) {
         return val;
     });
     var data = "";
-    /*data = fetch('').then((res) => {
-        return res.json();
-    });
-    if (data == ""){
-        data = fetch("../json/prodData.json").then((res) => {
-            return res.json();
-        }); //.then(d => {return JSON.stringify(d)});
-        //console.log(data);
-    }*/
     if (section == 0) {
-        //data = await fetch("https://gist.githubusercontent.com/narangopticals/ea3f7fa9a39a62872983e2b813441d53/raw/1c2b863b7e5b5f3c94e9f7bc51acf40e1828e19c/prodFmData.json").then((res) => {
-        var results = fetch('https://api.github.com/gists/5bd845b70c653deca357979be9e4fdd0').then(results => {
+        var results = fetch('https://api.github.com/gists/ea3f7fa9a39a62872983e2b813441d53').then(results => {
             return results.json();
         });
         var data = await results.then(data => {
@@ -80,25 +70,32 @@ export async function Func(section) {
         document.getElementById("mainProdView").innerHTML = pS;
     }
 }
+export async function msgWhatsapp(e){
+    var obj = e.currentTarget;
+    var text = obj.values[0];
+    console.log(text.length);
+    if(text.length > 0){
+        var devType = navigator.userAgent;
+        var wbLink = "";
+        if ( devType.match(/Android/i)
+            || devType.match(/webOS/i)
+            || devType.match(/iPhone/i)
+            || devType.match(/iPad/i)
+            || devType.match(/iPod/i)
+            || devType.match(/BlackBerry/i)
+            || devType.match(/Windows Phone/i) )
+        {
+            wbLink = "https://wa.me/+919756231332?text=";
+        } else {
+            wbLink = "https://api.whatsapp.com/send/?phone=919756231332&text=";
+        }
+        window.open(wbLink+text,"_blank");
+    }
+}
 export async function loadItems(startNum, endNum) {
     var elemsParents = window.document.querySelectorAll('div[class^=grid]');
     //console.log(elemsParents);
-    var i = startNum;
-    var devType = navigator.userAgent;
-    var wbLink = "";
-    if (devType.match(/Android/i)
-        || devType.match(/webOS/i)
-        || devType.match(/iPhone/i)
-        || devType.match(/iPad/i)
-        || devType.match(/iPod/i)
-        || devType.match(/BlackBerry/i)
-        || devType.match(/Windows Phone/i)) {
-        //wbLink = "https://api.whatsapp.com/send/?phone=919756231332&text=";
-        //wbLink = "intent:#Intent;scheme=startci://open?url_param="
-        wbLink = "https://wa.me/+919756231332?text=";
-    } else {
-        wbLink = "https://api.whatsapp.com/send/?phone=919756231332&text=";
-    }
+    var i = startNum;    
     for (var j = 0; j < 9; j++) {
         var elem = elemsParents[j];
         if (i < str.length) {
@@ -108,39 +105,26 @@ export async function loadItems(startNum, endNum) {
             //console.log(elem);
             elem.querySelector("iframe").src = valueData.imgfile;
             elem.querySelector("h1").textContent = valueData.title;
-            //elem.querySelector("a").href="https://wa.me/+919756231332?text="+encodeURIComponent(encodeURIComponent(valueData.title)+"\n Item ID"+i);
-            //elem.querySelector("a").href="https://api.whatsapp.com/send/?phone=919756231332&text="+encodeURIComponent(encodeURIComponent(valueData.title)+"\n Item ID"+i)+"&type=phone_number&app_absent=0";
-            elem.querySelector("a").href = wbLink + "I want to know More About Your Products" + encodeURIComponent("\nName : ") + encodeURIComponent(valueData.title) + encodeURIComponent(",  Item ID :") + valueData.itemcode + "&type=phone_number&app_absent=0&send=1";
-            //elem.querySelector("a").href = wbLink+"I want to know More About Your Products"+encodeURIComponent("\nName : ") + encodeURIComponent(valueData.title) + encodeURIComponent(",  Item ID :") + valueData.itemcode +";package=com.whatsapp;end"
+            var val =  [ encodeURIComponent("I want to know More About Your Products") +
+                        encodeURIComponent("\nName : ") +
+                        encodeURIComponent(valueData.title) +
+                        encodeURIComponent(",  Item ID :") +
+                        encodeURIComponent(valueData.itemnum) +
+                        "&type=phone_number&app_absent=0&send=1"];
+            elem.querySelector("button").values = val;
+            elem.querySelector("button").addEventListener( "click",
+                function(e){
+                    msgWhatsapp(e);
+                });
             i++;
             //elem.querySelector("h1").style.fontSize = "inherit";
         } else {
             elem.querySelector("iframe").src = "";
             elem.querySelector("h1").textContent = "";
             elem.style.visibility = "hidden";
-        } var devType = navigator.userAgent;
-        var wbLink = "";
-        if (devType.match(/Android/i)
-            || devType.match(/webOS/i)
-            || devType.match(/iPhone/i)
-            || devType.match(/iPad/i)
-            || devType.match(/iPod/i)
-            || devType.match(/BlackBerry/i)
-            || devType.match(/Windows Phone/i)) {
-            wbLink = "https://wa.me/+919756231332?text";
-        } else {
-            wbLink = "https://api.whatsapp.com/send/?phone=919756231332&text=";
         }
-        /*var currentPageNo = Math.round(startNum/9);
-        var buttons = document.getElementById('btnNavProdView').children;
-        for (i = 0 ; i < buttons.length ; i++){            
-            buttons[i].textContent = buttons[i].textContent.replaceAll('*','');
-            if (i == currentPageNo){
-                buttons[i].textContent = buttons[i].textContent + '*';
-            }
-        }*/
     }
-    removePopElem();
+    /*removePopElem();
     function removePopElem() {
         var popElem = document.querySelectorAll('.ndfHFb-c4YZDc-Wrql6b');
         for (var i = 0; i < popElem.length; i++) {
@@ -148,7 +132,7 @@ export async function loadItems(startNum, endNum) {
         }
         setTimeout(removePopElem, 15000);
         //console.log("popup found = "+popElem.length);
-    }
+    }*/
 
 
 }
