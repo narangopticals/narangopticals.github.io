80486268fe3466ef6617507d7941094101ba55e1
Nicx A <nicx92@gmail.com>
2023-11-07 20:02:38 +0530

    swipe hompage category

--- js/indexAnimate.js
+++ js/indexAnimate.js
@@ -105,7 +105,7 @@ async function initMain() {
 export async function loadAnimation() {
 
 }
-
+var autoSwipe = true;
 export async function animateFlexs() {
     let slideIndex = 0;
     //showSlides(slideIndex);
@@ -113,36 +113,39 @@ export async function animateFlexs() {
     //let boolPlay = false;
     //const slideshowContainer = document.getElementById("productIntroHolder");
     firstRun();
+    swipeFeature();
 
     var autoBol = false;
 
     function firstRun() {
         autoBol = false;
-        let i;
-        let slides = window.document.getElementsByClassName("typeHolder");
-        //let slideName = document.getElementsByClassName("slideName");
-        for (i = 0; i < slides.length; i++) {
-            /*var perc = ((i - slideIndex) * 100);
-            slides[i].style.left = + '%';
-            console.log("line 127: i =" + i + ", perc= " + perc + ", slideIndex=" + slideIndex);*/
-            //slides[i].style.position = 'relative';
-            slides[i].style.display = 'none';
+        if (autoSwipe) {
+            let i;
+            let slides = window.document.getElementsByClassName("typeHolder");
+            //let slideName = document.getElementsByClassName("slideName");
+            for (i = 0; i < slides.length; i++) {
+                /*var perc = ((i - slideIndex) * 100);
+                slides[i].style.left = + '%';
+                console.log("line 127: i =" + i + ", perc= " + perc + ", slideIndex=" + slideIndex);*/
+                //slides[i].style.position = 'relative';
+                slides[i].style.display = 'none';
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) { slideIndex = 1 }
+            //slides[slideIndex - 1].style.position = 'absolute';
+            var pr = 0;
+            /*if (window.screen.availHeight > window.screen.availWidth) {
+                pr = -((slideIndex - 1) * 100);
+            } else {
+                pr = (15 - ((slideIndex - 1) * 45));
+            }
+            slides[slideIndex - 1].style.left = pr + '%';
+            slides[slideIndex - 1].style.visibility = 'visible';
+            console.log("line 127:pr= " + pr + ", slideIndex=" + slideIndex);*/
+            slides[slideIndex - 1].style.display = '';
+            //slides[slideIndex - 1].style.top = '10%';
+            setTimeout(firstRun, 5000); // Change image every 2 seconds
         }
-        slideIndex++;
-        if (slideIndex > slides.length) { slideIndex = 1 }
-        //slides[slideIndex - 1].style.position = 'absolute';
-        var pr = 0;
-        /*if (window.screen.availHeight > window.screen.availWidth) {
-            pr = -((slideIndex - 1) * 100);
-        } else {
-            pr = (15 - ((slideIndex - 1) * 45));
-        }
-        slides[slideIndex - 1].style.left = pr + '%';
-        slides[slideIndex - 1].style.visibility = 'visible';
-        console.log("line 127:pr= " + pr + ", slideIndex=" + slideIndex);*/
-        slides[slideIndex - 1].style.display = '';
-        //slides[slideIndex - 1].style.top = '10%';
-        setTimeout(firstRun, 5000); // Change image every 2 seconds
     }
     function secondRun() {
         if (slideCount < 5) {
@@ -175,4 +178,73 @@ export async function animateFlexs() {
         /*        slides[slideIndex - 1].style.display = "block";*/
         //slideNames[slideIndex - 1].className += " active";
     }
+
+    function swipeFeature() {
+        var touchableElement = document.getElementById('productIntroHolder');
+        var catFrames = touchableElement.querySelectorAll('.typeHolder');
+        var framesTotal = catFrames.length;
+        var touchstartX, touchstartY, touchendX, touchendY;
+        touchableElement.addEventListener('touchstart', function (event) {
+            touchstartX = event.changedTouches[0].screenX;
+            touchstartY = event.changedTouches[0].screenY;
+        }, false);
+
+        touchableElement.addEventListener('touchend', function (event) {
+            touchendX = event.changedTouches[0].screenX;
+            touchendY = event.changedTouches[0].screenY;
+            autoSwipe = false;
+            setTimeout(handleGesture, 500);
+        }, false);
+
+
+        function handleGesture() {
+            var currentSlide = 0;
+            for (var i = 0; i < framesTotal; i++) {
+                if (catFrames[i].style.display == 'block') {
+                    currentSlide = i;
+                    break;
+                }
+            }
+            console.log('currentSlide:' + currentSlide);
+            if (touchendX < touchstartX) {
+                console.log('Swiped Left');
+                if (currentSlide >= 0 && currentSlide < framesTotal - 1) {
+                    catFrames[currentSlide].style.display = 'none';
+                    catFrames[currentSlide + 1].style.display = 'block';
+                } else if (currentSlide == (framesTotal - 1)) {
+                    catFrames[currentSlide].style.display = 'none';
+                    catFrames[0].style.display = 'block';
+                }
+                //autoSwipe = true;
+                //setTimeout(firstRun, 5000);
+            }
+
+            if (touchendX > touchstartX) {
+                console.log('Swiped Right');
+
+                if (currentSlide > 0 && currentSlide < framesTotal) {
+                    catFrames[currentSlide].style.display = 'none';
+                    catFrames[currentSlide - 1].style.display = 'block';
+                } else if (currentSlide == 0) {
+                    catFrames[currentSlide].style.display = 'none';
+                    catFrames[framesTotal - 1].style.display = 'block';
+                }
+                //autoSwipe = true;
+                //setTimeout(firstRun, 5000);
+            }
+
+            if (touchendY < touchstartY) {
+                console.log('Swiped Up');
+            }
+
+            if (touchendY > touchstartY) {
+                console.log('Swiped Down');
+            }
+
+            if (touchendY === touchstartY) {
+                console.log('Tap');
+            }
+        }
+    }
+
 }
